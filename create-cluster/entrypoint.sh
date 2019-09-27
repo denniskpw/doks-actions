152999df76cfdbc38106921b9add479620c498cc
Dennis Wong <dwong@digitalocean.com>
2019-09-27 13:27:03 -0400

    Make cluster information available for later steps

--- create-cluster/entrypoint.sh
+++ create-cluster/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -l
 
-
-# echo "Creating droplet"
-# /app/doctl compute droplet create foobar --region nyc3 --image ubuntu-18-04-x64 --size s-1vcpu-1gb
+set -euo pipefail
 
 RANDOM_STRING=$(hexdump -n 12 -e '3/4 "%08X" 1 "\n"' /dev/random)
 NAME=test-cluster-$(echo ${RANDOM_STRING} | tr '[:upper:]' '[:lower:]')
 
 echo "Creating k8s cluster: ${NAME}"
-/app/doctl kubernetes cluster create ${NAME}
-/app/doctl kubernetes cluster kubeconfig save ${NAME}
+/app/doctl kubernetes cluster create ${NAME} --update-kubeconfig=false
 
-ls -al /github/home/.kube
+CLUSTER_ID=$(/app/doctl kubernetes cluster get ${NAME} | grep ${NAME} | awk '{print $1}')
+/app/doctl kubernetes cluster kubeconfig show ${NAME} > $GITHUB_WORKSPACE/kubeconfig
+ls -al $GITHUB_WORKSPACE
+
+echo "::set-output name=CLUSTER_NAME::${NAME}"
+echo "::set-output name=CLUSTER_ID::${CLUSTER_ID}"
